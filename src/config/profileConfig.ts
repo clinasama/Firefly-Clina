31581f1de54d06091080598deba7a73b52773fee
clinasama <184842861+clinasama@users.noreply.github.com>
2026-09-16 21:52:00 +0800

    chore:update profile

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://image.clina.top/images/avatar.webp",
 
 	// 名字
-	name: "XiaYe",
+	name: "ClinaSaMa",
 
 	// 个人签名
-	bio: "Hello, I'm XiaYe.",
+	bio: "¡El pueblo unido jamás será vencido!",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,13 +24,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/ClinaSaMa",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:clina@disroot.org",
 			showName: false,
 		},
 		{
